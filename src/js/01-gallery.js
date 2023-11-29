05b96736695dfeb1d712ea497322c051a7ec97f1
Paulina Mlynarska <paulinamlynarska@outlook.com>
2023-11-29 18:23:44 +0000

    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,4 +5,29 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryImages = galleryItems
+  .map(
+    img =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+  </li>`
+  )
+  .join('');
+
+gallery.innerHTML = galleryImages;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
